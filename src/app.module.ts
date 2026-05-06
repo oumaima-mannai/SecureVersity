f5e3caa54f8aeb1a5f21319437bf15dc178a10b1
oumaima-mannai <manaoumaima28@gmail.com>
2026-05-06 17:58:54 +0100

    feat: core dependencies, prisma schema updates, and main app module configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,17 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TemplatesModule } from './modules/templates/templates.module';
+import { AssessmentsModule } from './modules/assessments/assessments.module';
+import { ResponsesModule } from './modules/responses/responses.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule, 
     UsersModule, 
-    AuthModule
+    AuthModule, TemplatesModule, AssessmentsModule, ResponsesModule, NotificationsModule
   ],
   controllers: [AppController],
   providers: [AppService],
