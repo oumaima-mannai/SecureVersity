bca6001a764e798e806017d9af7dee262b494f66
oumaima-mannai <manaoumaima28@gmail.com>
2026-05-06 17:41:22 +0100

    feat: core dependencies, routing, and global styling updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,11 @@ export const routes: Routes = [
       import('./pages/login/login.component').then(m => m.LoginComponent),
   },
   {
+    path: 'change-password',
+    loadComponent: () =>
+      import('./pages/change-password/change-password').then(m => m.ChangePasswordComponent),
+  },
+  {
     path: 'audit',
     loadComponent: () =>
       import('./pages/audit/audit.component').then(m => m.AuditComponent),
@@ -41,6 +46,17 @@ export const routes: Routes = [
         path: 'create-user',
         loadComponent: () => import('./pages/admin/create-user/create-user').then(m => m.CreateUser)
       }
+    ]
+  },
+  {
+    path: 'assessments',
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', loadComponent: () => import('./pages/assessments/dashboard/dashboard').then(m => m.Dashboard) },
+      { path: 'assign', loadComponent: () => import('./pages/assessments/assignment/assignment').then(m => m.Assignment) },
+      { path: 'tasks', loadComponent: () => import('./pages/assessments/tasks/tasks').then(m => m.Tasks) },
+      { path: 'play', loadComponent: () => import('./pages/assessments/questionnaire/questionnaire').then(m => m.Questionnaire) },
+      { path: 'validation', loadComponent: () => import('./pages/assessments/validation/validation').then(m => m.Validation) }
     ]
   },
   { path: '**', redirectTo: '' },
